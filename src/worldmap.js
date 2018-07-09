0917db9f49f3b0e495e1a22553c8c2c173caf136
clompsy <cppr@gmx.de>
2018-07-09 21:41:59 +0200

    Correct usage of options to set zoom with the center to avoid recentering to 0,0 when loading panel.

--- src/worldmap.js
+++ src/worldmap.js
@@ -19,10 +19,7 @@ export default class WorldMap {
 
   createMap() {
     const mapCenter = window.L.latLng(parseFloat(this.ctrl.panel.mapCenterLatitude), parseFloat(this.ctrl.panel.mapCenterLongitude));
-    this.map = window.L.map(this.mapContainer, {worldCopyJump: true, center: mapCenter})
-      .fitWorld()
-      .zoomIn(parseInt(this.ctrl.panel.initialZoom, 10));
-    this.map.panTo(mapCenter);
+    this.map = window.L.map(this.mapContainer, { worldCopyJump: true, center: mapCenter, zoom: (this.panel.initialZoom || 1) });
     this.setMouseWheelZoom();
 
     const selectedTileServer = tileServers[this.ctrl.tileServer];
