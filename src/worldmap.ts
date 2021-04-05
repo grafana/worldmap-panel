dec89a1a88d85ac1fac60d2660ac8b797b1f9e40
Kranthi_Tatikonda_LDDS <kt031855@broadcom.com>
2021-04-05 15:10:12 +0530

    Regex changes

--- src/worldmap.ts
+++ src/worldmap.ts
@@ -4,19 +4,21 @@ import WorldmapCtrl from './worldmap_ctrl';
 
 const tileServers = {
   'CartoDB Positron': {
-    url: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+    url:
+      'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> ' +
       '&copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-    subdomains: 'abcd',
+    subdomains: 'abcd'
   },
   'CartoDB Dark': {
-    url: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+    url:
+      'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> ' +
       '&copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-    subdomains: 'abcd',
-  },
+    subdomains: 'abcd'
+  }
 };
 
 export default class WorldMap {
@@ -26,11 +28,14 @@ export default class WorldMap {
   map: any;
   legend: any;
   circlesLayer: any;
+  aggregations: any;
+  templateSrv: any;
 
-  constructor(ctrl, mapContainer) {
+  constructor(ctrl, mapContainer, templateSrv?) {
     this.ctrl = ctrl;
     this.mapContainer = mapContainer;
     this.circles = [];
+    this.templateSrv = templateSrv;
   }
 
   createMap() {
@@ -42,7 +47,7 @@ export default class WorldMap {
       worldCopyJump: true,
       preferCanvas: true,
       center: mapCenter,
-      zoom: parseInt(this.ctrl.panel.initialZoom, 10) || 1,
+      zoom: parseInt(this.ctrl.panel.initialZoom, 10) || 1
     });
     this.setMouseWheelZoom();
 
@@ -52,7 +57,7 @@ export default class WorldMap {
       subdomains: selectedTileServer.subdomains,
       reuseTiles: true,
       detectRetina: true,
-      attribution: selectedTileServer.attribution,
+      attribution: selectedTileServer.attribution
     }).addTo(this.map);
   }
 
@@ -80,11 +85,61 @@ export default class WorldMap {
           this.ctrl.panel.colors[index + 1] +
           '"></i> ' +
           thresholds[index] +
-          (thresholds[index + 1] ? '&ndash;' + thresholds[index + 1] + '</div>' : '+');
+          (thresholds[index + 1]
+            ? '&ndash;' + thresholds[index + 1] + '</div>'
+            : '+');
       }
       this.legend._div.innerHTML = legendHtml;
     };
     this.legend.addTo(this.map);
+  }
+
+  createAggregations() {
+    this.aggregations = (<any>window).L.control({ position: 'bottomright' });
+    this.aggregations.onAdd = () => {
+      this.aggregations._div = (<any>window).L.DomUtil.create(
+        'div',
+        'info aggregations'
+      );
+      this.aggregations.update();
+      return this.aggregations._div;
+    };
+
+    this.aggregations.update = () => {
+      const aggregations = this.ctrl.data.aggregations || {};
+      let aggregationsHtml = '';
+      if (Object.keys(aggregations).length <= 1 && aggregations.unknown === 0) {
+        aggregationsHtml = '<div>No Data Available</div>';
+      } else if (
+        this.ctrl.data.aggregationSortedList &&
+        this.ctrl.data.aggregationSortedList.length &&
+        Object.keys(aggregations).length > 1
+      ) {
+        this.ctrl.data.aggregationSortedList.forEach(agg => {
+          if (aggregations[agg] !== 0) {
+            aggregationsHtml = `${aggregationsHtml}
+          <div class="agg-item">
+            <div>
+              <div class="agg-label" title=${agg}>${agg}</div>
+              <b>${(100 * (aggregations[agg] / this.ctrl.data.length))
+                .toFixed(2)
+                .replace(/[.,]00$/, '')}%</b>
+            </div>
+            <div class="agg-bar">
+              <div class="agg-bar-progress" style="width: ${100 *
+              (aggregations[agg] / this.ctrl.data.length)}%">
+                progress
+              </div>
+            </div>
+          </div>`;
+          }
+        });
+      } else {
+        aggregationsHtml = `Invalid mapping for aggregations`;
+      }
+      this.aggregations._div.innerHTML = `<div class="agg-container">${aggregationsHtml}</div>`;
+    };
+    this.aggregations.addTo(this.map);
   }
 
   needToRedrawCircles(data) {
@@ -103,7 +158,10 @@ export default class WorldMap {
 
   filterEmptyAndZeroValues(data) {
     return _.filter(data, o => {
-      return !(this.ctrl.panel.hideEmpty && _.isNil(o.value)) && !(this.ctrl.panel.hideZero && o.value === 0);
+      return (
+        !(this.ctrl.panel.hideEmpty && _.isNil(o.value)) &&
+        !(this.ctrl.panel.hideZero && o.value === 0)
+      );
     });
   }
 
@@ -117,11 +175,17 @@ export default class WorldMap {
 
   drawCircles() {
     const data = this.filterEmptyAndZeroValues(this.ctrl.data);
-    if (this.needToRedrawCircles(data)) {
+    this.clearCircles();
+    this.createCircles(data);
+    /*if (this.needToRedrawCircles(data)) {
       this.clearCircles();
       this.createCircles(data);
     } else {
       this.updateCircles(data);
+    }*/
+    // Update Aggregations
+    if (this.aggregations && this.aggregations !== null) {
+      this.aggregations.update();
     }
   }
 
@@ -131,7 +195,7 @@ export default class WorldMap {
       if (!dataPoint.locationName) {
         return;
       }
-      circles.push(this.createCircle(dataPoint));
+      circles.push(this.createCircle(dataPoint, data));
     });
     this.circlesLayer = this.addCircles(circles);
     this.circles = circles;
@@ -148,66 +212,120 @@ export default class WorldMap {
       });
 
       if (circle) {
+        circle.options.dataset = data;
         circle.setRadius(this.calcCircleSize(dataPoint.value || 0));
         circle.setStyle({
           color: this.getColor(dataPoint.value),
           fillColor: this.getColor(dataPoint.value),
           fillOpacity: 0.5,
-          location: dataPoint.key,
+          location: dataPoint.key
         });
         circle.unbindPopup();
-        this.createPopup(circle, dataPoint.locationName, dataPoint.valueRounded);
+        this.createPopup(
+          circle,
+          dataPoint.locationName,
+          dataPoint.valueRounded,
+          dataPoint.isMetricFieldNaN
+        );
       }
     });
+    if (this.aggregations && this.aggregations !== null) {
+      this.aggregations.update();
+    }
   }
 
-  createCircle(dataPoint) {
-    const circle = (<any>window).L.circleMarker([dataPoint.locationLatitude, dataPoint.locationLongitude], {
-      radius: this.calcCircleSize(dataPoint.value || 0),
-      color: this.getColor(dataPoint.value),
-      fillColor: this.getColor(dataPoint.value),
-      fillOpacity: 0.5,
-      location: dataPoint.key,
-    });
-
-    this.createPopup(circle, dataPoint.locationName, dataPoint.valueRounded);
+  createCircle(dataPoint, dataset?) {
+    const circle = (<any>window).L.circleMarker(
+      [dataPoint.locationLatitude, dataPoint.locationLongitude],
+      {
+        radius: this.calcCircleSize(dataPoint.value || 0),
+        color: this.getColor(dataPoint.value),
+        fillColor: this.getColor(dataPoint.value),
+        fillOpacity: 0.5,
+        location: dataPoint.key,
+        dataset
+      }
+    );
+    this.createPopup(
+      circle,
+      dataPoint.locationName,
+      dataPoint.valueRounded,
+      dataPoint.isMetricFieldNaN
+    );
     return circle;
   }
 
   calcCircleSize(dataPointValue) {
-    const circleMinSize = parseInt(this.ctrl.panel.circleMinSize, 10) || 2;
-    const circleMaxSize = parseInt(this.ctrl.panel.circleMaxSize, 10) || 30;
+    const circleMinSize = parseInt(this.ctrl.panel.circleMinSize, 10) || 10;
+    const circleMaxSize = parseInt(this.ctrl.panel.circleMaxSize, 10) || 10;
 
     if (this.ctrl.data.valueRange === 0) {
       return circleMaxSize;
     }
 
-    const dataFactor = (dataPointValue - this.ctrl.data.lowestValue) / this.ctrl.data.valueRange;
+    const dataFactor =
+      (dataPointValue - this.ctrl.data.lowestValue) / this.ctrl.data.valueRange;
     const circleSizeRange = circleMaxSize - circleMinSize;
 
     return circleSizeRange * dataFactor + circleMinSize;
   }
 
-  createPopup(circle, locationName, value) {
-    const unit = value && value === 1 ? this.ctrl.panel.unitSingular : this.ctrl.panel.unitPlural;
-    const label = (locationName + ': ' + value + ' ' + (unit || '')).trim();
-    circle.bindPopup(label, {
-      offset: (<any>window).L.point(0, -2),
-      className: 'worldmap-popup',
-      closeButton: this.ctrl.panel.stickyLabels,
-    });
+  createPopup(circle, locationName, value, isMetricFieldNaN) {
+    const unit =
+      value && value === 1
+        ? this.ctrl.panel.unitSingular
+        : this.ctrl.panel.unitPlural;
 
-    circle.on('mouseover', function onMouseOver(evt) {
-      const layer = evt.target;
-      layer.bringToFront();
-      this.openPopup();
-    });
+    let formattedLocationList = '';
+    circle.options.dataset
+      .filter(data => data.key === circle.options.location)
+      .forEach(location => {
+        if (
+          this.ctrl.panel.drilldownTarget &&
+          this.ctrl.panel.drilldownTarget !== ''
+        ) {
+          const url = this.ctrl.panel.drilldownTarget
+            ? this.ctrl.panel.drilldownTarget.split('${__locationName}').join(`${location.locationName}`) : '';
+          const hrefUrl = this.templateSrv.replace(url);
 
-    if (!this.ctrl.panel.stickyLabels) {
-      circle.on('mouseout', function onMouseOut() {
-        circle.closePopup();
+          formattedLocationList = `${formattedLocationList}
+            <div>
+            ${`
+            ${
+              this.ctrl.panel.drillDownTab
+                ? `<a href="${hrefUrl}" target="_blank">${location.locationName}</a> `
+                : `<a href="${hrefUrl}">${location.locationName}</a>`
+              }
+            ${!isMetricFieldNaN ? `: ${location.value}` : ''}
+            ${unit ? `(${unit})` : ''}`.trim()}
+            </div>`;
+        } else {
+          formattedLocationList = `${formattedLocationList} <div>
+           ${`${location.locationName} ${
+              !isMetricFieldNaN ? `: ${location.value}` : ''
+              }
+           ${unit ? `(${unit})` : ''}`.trim()}
+           </div>`;
+        }
+        const label = `<div class="tooltip-container">${formattedLocationList}</div>`;
+        circle.bindPopup(label, {
+          offset: (<any>window).L.point(0, -2),
+          className: 'worldmap-popup',
+          closeButton: this.ctrl.panel.stickyLabels
+        });
+
+        circle.on('mouseover', function onMouseOver(evt) {
+          const layer = evt.target;
+          layer.bringToFront();
+          this.openPopup();
+        });
+
+        if (!this.ctrl.panel.stickyLabels) {
+          circle.on('mouseout', function onMouseOut() {
+            circle.closePopup();
+          });
+        }
       });
-    }
   }
 
   getColor(value) {
@@ -224,13 +342,21 @@ export default class WorldMap {
   }
 
   panToMapCenter() {
-    this.map.panTo([parseFloat(this.ctrl.panel.mapCenterLatitude), parseFloat(this.ctrl.panel.mapCenterLongitude)]);
+    this.map.panTo([
+      parseFloat(this.ctrl.panel.mapCenterLatitude),
+      parseFloat(this.ctrl.panel.mapCenterLongitude)
+    ]);
     this.ctrl.mapCenterMoved = false;
   }
 
   removeLegend() {
     this.legend.remove(this.map);
     this.legend = null;
+  }
+
+  removeAggregations() {
+    this.aggregations.remove(this.map);
+    this.aggregations = null;
   }
 
   setMouseWheelZoom() {
@@ -260,6 +386,9 @@ export default class WorldMap {
     }
     if (this.legend) {
       this.removeLegend();
+    }
+    if (this.aggregations) {
+      this.removeAggregations();
     }
     this.map.remove();
   }
